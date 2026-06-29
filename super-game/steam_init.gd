507bc25b55e96bf6d3aca1911d6cb278e8556d02
hilloftheking <91853499+hilloftheking@users.noreply.github.com>
2026-06-29 03:01:16 -0400

    Add GodotSteam and main scene

---
+++ super-game/steam_init.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Space War AppID lets us use Steam P2P lobbies for free
+const APP_ID := 480
+
+var is_steam_initialized := false
+
+
+func _ready() -> void:
+	is_steam_initialized = Steam.steamInit(APP_ID, true)
+	print(is_steam_initialized)
