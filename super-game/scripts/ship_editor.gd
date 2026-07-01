6064270e92bd2c72be993e160b206263657d0e1f
aceofdiamonds4500 <tacoparrot4500@gmail.com>
2026-07-01 02:16:15 -0400

    Add ship editor concept

---
+++ super-game/scripts/ship_editor.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@export var body_tex: Texture2D
+@export var rocket_tex: Texture2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
